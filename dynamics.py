d511be26d8d6f260b0ff6f263d579a2a77a0cf61
Jonathan Curtis <jon.curtis.94@gmail.com>
2023-01-11 19:48:18 -0500

    Dynamics produces correlation functions

--- dynamics.py
+++ dynamics.py
@@ -5,14 +5,13 @@
 import numpy as np
 from matplotlib import pyplot as plt 
 
-L = 20### Lattice size -- LxL lattice
+L = 40### Lattice size -- LxL lattice
 
 J = 1.### Phase stiffness in units of Kelvin
-Gamma = 1.### Phase diffusivity (unitless) (can be taken to one, setting the size of time step)
-T = 0.2### Temperature in units of Kelvin
+T = .7### Temperature in units of Kelvin
 
-dt = .0005### Time step (must be very small)
-ntimes = 1000### Number of times steps we calculate
+dt = .001### Time step (must be very small) 
+ntimes = 5000### Number of times steps we calculate
 
 thetas = np.zeros((ntimes,L,L))
 #thetas[0,:,:] = ( np.random.default_rng().uniform(-np.pi,np.pi,L*L) ).reshape((L,L))
@@ -26,21 +25,22 @@ for nt in range(1,ntimes):
 	for k in range(L*L):
 		x = k//L
 		y = k % L
-		thetas[nt,x,y] = thetas[nt-1,x,y] - J*dt*(
-			np.sin( thetas[nt-1,x,y] - thetas[nt-1,(x+1)//L,y]) 
-			+np.sin( thetas[nt-1,x,y] - thetas[nt-1,x-1,y]) 
-			+np.sin( thetas[nt-1,x,y] - thetas[nt-1,x,(y+1)//L]) 
-			+np.sin( thetas[nt-1,x,y] - thetas[nt-1,x,y-1])
-			)/Gamma
-		thetas[nt,x,y] += dt*( np.random.default_rng().normal(0.,2.*T*Gamma/dt)) /Gamma
+		###thetas[nt,x,y] = thetas[nt-1,x,y] - J*dt*(
+		###	np.sin( thetas[nt-1,x,y] - thetas[nt-1,(x+1)//L,y]) 
+		###	+np.sin( thetas[nt-1,x,y] - thetas[nt-1,x-1,y]) 
+		###	+np.sin( thetas[nt-1,x,y] - thetas[nt-1,x,(y+1)//L]) 
+		###	+np.sin( thetas[nt-1,x,y] - thetas[nt-1,x,y-1])
+		###	)
+		thetas[nt,x,y] = thetas[nt-1,x,y]- J * dt *np.sin(thetas[nt-1,x,y])
+		thetas[nt,x,y] +=  np.random.default_rng().normal(0.,2.*T*dt)
 
 
 ### This will calculate the correlation function < e^{itheta(0) - i theta(x)}> for each time, summed over sites in the lattice
 
-Gx = np.zeros((ntimes,L),dtype=complex)
-for nt in rangE(ntimes):
-	for nx in range(L):
-		Gx[nt,nx] = np.mean( np.exp( 1.j*thetas[nt,0,:] - 1.j*thetas[nt,nx,:] ) )
+Gx = np.zeros((ntimes,L+1),dtype=complex)
+for nt in range(ntimes):
+	for nx in range(L+1):
+		Gx[nt,nx] = np.mean( np.exp( 1.j*(thetas[nt,nx % L,:] - thetas[nt,0,:]) ) )
 
 
 nt = 0
@@ -49,9 +49,10 @@ while nt < ntimes:
 	plt.show()
 
 
-	nt += 100
+	nt += 500
 
-
+plt.plot(np.abs(np.mean(Gx,axis=0)))
+plt.show()
 
 
 
