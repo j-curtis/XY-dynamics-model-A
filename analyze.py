db77e436baa945d21b1808dd27141e4d1a8fcc18
Jonathan Curtis <jon.curtis.94@gmail.com>
2023-02-21 22:46:51 -0500

    Code to save vorticity data

--- analyze.py
+++ analyze.py
@@ -14,32 +14,62 @@ def csv2npy(fname):
 	np.save(fname+".npy",data)
 
 
-def plot(fname):
+def plotthetas(fname):
 
-	L = 500### Lattice size -- LxL lattice
+	#L = 200### Lattice size -- LxL lattice
 	J = 1.### Phase stiffness in units of Kelvin
-	T = .5*J ### Literature says BKT transition is at approximately .89 J 
-	#ntimes = 5000
-	dt = .05
+	T = 1.*J ### Literature says BKT transition is at approximately .89 J 
+	#ntimes = 20000
+	dt = .05*J
 
 	data = np.load(fname+".npy")
+	nt = data.shape[0]
+	L = data.shape[1]
 
 	Gxmean = np.zeros(L,dtype=complex)
-	Gxvar = np.zeros(L,dtype = complex)
+	#Gxvar = np.zeros(L,dtype = complex)
 
-	for n in range(L):
-		Gxmean[n] = np.mean(np.exp(1.j*(data[:,n,0] - data[:,0,0] ) )  ,axis=0)
-		Gxvar[n] = np.var(np.exp(1.j*(data[:,n,0] - data[:,0,0] ) )  ,axis=0)
+	Gxmean = np.mean(np.exp(1.j*(data[:,0,:] - np.outer(data[:,0,0],np.ones(L) ) ) )  ,axis=0)
 
-	#Gx = np.mean(np.exp(1.j*(data[:,:,0] - np.outer(data[:,0,0],np.ones(L) ) ) ) ,axis=0)
+	#plt.plot(np.abs(Gxmean))
+	#plt.show()
 
-	plt.plot(np.abs(Gxmean)**2)
+	Gtmean = np.mean(np.exp(1.j*(data[:,:,:] - np.tensordot(np.ones(nt),data[0,:,:],axes=0) ) )  ,axis=(-1,-2))
+	plt.plot(np.abs(Gtmean))
+	plt.show()
+
+def plotMs(fname):
+	L = 300
+	J = 1.
+	dt = .05 
+
+	data = np.genfromtxt(fname+".csv",delimiter=", ")
+	ntemps = data.shape[0]
+	temps = data[:,0]
+	m = data[:,1]**2 + data[:,2]**2
+	plt.plot(temps,m)
+	plt.show()
+
+def plotvorticity(fname):
+	J = 1.### Phase stiffness in units of Kelvin
+	T = 1.*J ### Literature says BKT transition is at approximately .89 J 
+	#ntimes = 20000
+	dt = .05*J
+
+	data = np.load(fname+".npy")
+	nt = data.shape[0]
+	L = data.shape[1]
+
+	plt.imshow(data[0,:,:])
+	plt.colorbar()
 	plt.show()
 
 def main():
-	fname = "thetas"
+	fname = "vorticity"
+	#plotMs(fname)
 	csv2npy(fname)
-	plot(fname)
+	#plotthetas(fname)
+	plotvorticity(fname)
 
 if __name__ == "__main__":
 	main()
--- dynamics.cpp
+++ dynamics.cpp
@@ -64,46 +64,47 @@ double* OPav(size_t L, size_t ntimes, double T, double J){
 
 int main() {
 
-	const size_t L = 300;
+	const size_t L = 200;
 	const size_t ntimes = 5000;
 
-
+	//Measure energy in units of J
 	double J = 1.;
-	const size_t ntemps = 12;
+	double dt = .05/J;
+
+	//Different temperatures
+	/*
+	const size_t ntemps = 10;
 	double temps[ntemps];
 	for(int i =0; i < ntemps; i++){
-		temps[i] = 1.8*double(i+1)*J/double(ntemps);
+		temps[i] = (.5  + double(i+1)/double(ntemps))*J;
 	}
+	*/
+	//Set temperature
+	double T = 4.*J;
 
-	double dt = .05/J;
    	int t0 = std::time(NULL);
-
-	double mag[ntemps][2]; 
+   	/*
+   	//Run loop to collect M(T)
+ 	double mag[ntemps][2]; 
 	for(int i =0; i < ntemps; ++i){
 		double * tmp = OPav(L,ntimes,temps[i],J);
 		mag[i][0] = tmp[0];
 		mag[i][1] = tmp[1];
 	}
-	int tf = std::time(NULL);
+	*/
+	
+	//Loop to generate full spacetime data for theta(x,t)
 
-	std::cout<<(tf-t0)<<" seconds elapsed"<<std::endl;
+	//Initialize RNG
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  	std::default_random_engine generator (seed);
+  	std::normal_distribution<double> normal(0.0,sqrt(2.*T*dt));
 
-	std::ofstream outfile;
-	outfile.open("./M_vs_T.csv");
-	for(int i =0; i < ntemps; i++){
-		outfile<<temps[i]<<", "<<mag[i][0]<<", "<<mag[i][1]<<std::endl;
-	}
-	outfile.close();
-	/*
-
-	std::default_random_engine generator;
-  	std::normal_distribution<double> normal(0.0,2.*T*dt);
-
+  	//Array of theta(x,t) values
    	std::vector<std::vector<std::vector<double> > > thetas(ntimes, std::vector<std::vector<double> >(L, std::vector<double>(L) ) );
-
-   	int t0 = std::time(NULL);
-
-   	//We first run a burn run to thermalize the system
+	
+	//Burn loop
+	//We first run a burn run to thermalize the system
 	for(int nt = 1; nt < ntimes; ++nt){
 		for(int nx = 0; nx < L; nx++){
 			for(int ny = 0; ny <L; ny++){
@@ -119,7 +120,7 @@ int main() {
 				thetas[nt][nx][ny] += normal(generator);
 
 				//Now bring back to interval [-pi,pi]
-				thetas[nt][nx][ny] = std::fmod(thetas[nt][nx][ny] , 2.*pi) - pi;
+				thetas[nt][nx][ny] = std::fmod(thetas[nt][nx][ny] , 2.*pi);
 
 			}
 		}
@@ -132,7 +133,8 @@ int main() {
 		}
 	}
 
-	//Repeat; sample dynamics from this batch
+	//Sample loop
+	//Now we collect the data and store as a csv file
 	for(int nt = 1; nt < ntimes; ++nt){
 		for(int nx = 0; nx < L; nx++){
 			for(int ny = 0; ny <L; ny++){
@@ -148,22 +150,62 @@ int main() {
 				thetas[nt][nx][ny] += normal(generator);
 
 				//Now bring back to interval [-pi,pi]
-				thetas[nt][nx][ny] = std::fmod(thetas[nt][nx][ny] , 2.*pi) - pi;
+				thetas[nt][nx][ny] = std::fmod(thetas[nt][nx][ny] , 2.*pi);
 
 			}
 		}
 	}
 
-	int tf = std::time(NULL);
+	/*
+	//We now compute the correlation function < e^i(theta(x,t)-theta(0))>
+	const size_t ntd = 2000; //Number of time steps we evaluate the correlation delay function for 
+   	std::vector<std::vector<double> > costx(ntd, std::vector<double>(L) );
 
-	std::cout<<(tf-t0)<<" seconds elapsed"<<std::endl;
+   	for(int nt =0; nt < ntd; nt++){
+   		for(int nx = 0; nx < L; nx++){
+   			for(int i =0; i < L ; i ++){
+   				costx[nt][nx] += cos(thetas[nt][nx][i] - thetas[nt][nx][0])/double(L);
+   			}
+   		}
+   	}
+   	*/
 
-	//We only output sampled thetas from every few steps to increase ergodicity and efficiency
-	int samplestep = 500;
 
+	
+	//We now compute the time-traces of the vorticity
+	//We define the vorticity for a site r such that v(r) = (theta(r+x)-theta(r))mod 2pi + (theta(r+x+y)-theta(r+x))mod 2pi  +(theta(r+y)-theta(r+x+y))mod 2pi  +(theta(r)-theta(r+y))mod 2pi 
+   	std::vector<std::vector<std::vector<double> > > vort(ntimes, std::vector<std::vector<double> >(L, std::vector<double>(L) ) );
+
+   	for(int t =0; t < ntimes; t++){
+   		for(int x = 0; x < L; x++){
+   			for(int y =0; y< L ; y++){
+   				vort[t][x][y] = std::fmod(thetas[t][(x+1)%L][y] - thetas[t][x][y],2.*pi);
+   				vort[t][x][y] += std::fmod(thetas[t][(x+1)%L][(y+1)%L] - thetas[t][(x+1)%L][y],2.*pi);
+   				vort[t][x][y] += std::fmod(thetas[t][x][(y+1)%L] - thetas[t][(x+1)%L][(y+1)%L],2.*pi);
+   				vort[t][x][y] += std::fmod(thetas[t][x][y] - thetas[t][x][(y+1)%L],2.*pi);
+
+   			}
+   		}
+   	}
+
+	//Save vorticity to csv file (large data set)
+	std::ofstream outfile;
+	outfile.open("./vorticity.csv");
+	for(int t = 0; t < ntimes; t++){
+		for(int x =0; x < L; x++){
+			for(int y = 0; y < L; y++){
+				outfile<<" "<<vort[t][x][y];
+			}
+		}
+		outfile<<std::endl;
+	}
+	outfile.close();
+
+   	/*
+	//Save thermalized theta(t,x) to csv file (large data set)
 	std::ofstream outfile;
 	outfile.open("./thetas.csv");
-	for(int nt = 0; nt < ntimes; nt += samplestep){
+	for(int nt = 0; nt < ntimes; nt++){
 		for(int nx =0; nx < L; nx++){
 			for(int ny = 0; ny < L; ny++){
 				outfile<<" "<<thetas[nt][nx][ny];
@@ -173,7 +215,20 @@ int main() {
 	}
 	outfile.close();
 	*/
+	
+	//Save M(T) to csv file
+	/*
+	std::ofstream outfile;
+	outfile.open("./M_vs_T.csv");
+	for(int i =0; i < ntemps; i++){
+		outfile<<temps[i]<<", "<<mag[i][0]<<", "<<mag[i][1]<<std::endl;
+	}
+	outfile.close();
+	*/
+	int tf = std::time(NULL);
+	std::cout<<(tf-t0)<<" seconds elapsed"<<std::endl;
 
+	
 	return 0;
 }
 
